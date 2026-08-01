0b8f8e4cad9bdbdc9146aaacdb5e3808380ef9c3
nsaya0607ta-lab <n.saya0607.ta@gmail.com>
2026-08-01 22:39:05 +0900

    旧PDF処理を外して株式ニュース専用シェルへ切替

--- pdf-manager/src/app/page.tsx
+++ pdf-manager/src/app/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 
-import { AppShell } from '@/components/AppShell';
-import { FolderRuleRunner } from '@/components/FolderRuleRunner';
-import { GeminiGeneratedPdfImporter } from '@/components/GeminiGeneratedPdfImporter';
-import { IonqReportImporter } from '@/components/IonqReportImporter';
 import { ServiceWorkerUpdater } from '@/components/ServiceWorkerUpdater';
+import { StockNewsAppShell } from '@/components/stock/StockNewsAppShell';
 import { AppProvider } from '@/store/AppStore';
 import { CloudProvider } from '@/store/CloudStore';
 import { StockProvider } from '@/store/StockStore';
@@ -15,11 +12,7 @@ export default function Page() {
       <CloudProvider>
         <StockProvider>
           <ServiceWorkerUpdater />
-          <FolderRuleRunner />
-          <IonqReportImporter />
-          <GeminiGeneratedPdfImporter />
-          {/* Geminiの資料作成画面は AppShell 内で設定画面から開く */}
-          <AppShell />
+          <StockNewsAppShell />
         </StockProvider>
       </CloudProvider>
     </AppProvider>
